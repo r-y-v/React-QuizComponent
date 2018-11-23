b0eb0b0c47ccbb9f404ee0e3c68d290d37d5a2de
ryv <reshmiyeldo@outlook.com>
2018-11-23 20:02:11 +0530

    spelling

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,14 +12,14 @@ class Quiz extends Component {
     showNextQuestion() {
         this.setState((state) => { return { quiz_position: state.quiz_position + 1}})
     }
-    
+
     render() {
         const isQuizEnd = ((this.state.quiz_position -1) === quizData.quiz_questions.length)
         return (
             <div>
             { isQuizEnd ? <QuizEnd/> :
             <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}
-                showNextQuestionHanlder={this.showNextQuestion.bind(this)}
+                showNextQuestionHandler={this.showNextQuestion.bind(this)}
             />
             }
             </div>
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,7 @@ import QuizQuestionButton from './QuizQuestionButton'
 class QuizQuestion extends Component {
     handleClick(buttonText) {
         if(buttonText === this.props.quiz_question.answer) {
-            this.props.showNextQuestionHanlder()
+            this.props.showNextQuestionHandler()
         }
     }
     render() {
