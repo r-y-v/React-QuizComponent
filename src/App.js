e3dcf576e84bc0ab942fb3809e062cca4113f1f3
ryv <reshmiyeldo@outlook.com>
2018-11-23 18:19:00 +0530

    Module1
    check in module 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
+import Quiz from './Quiz'
+//import { start } from 'repl';
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz/>
     )
   }
 }
